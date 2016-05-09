78712a60b64ec230c31d2d44e19e5f263126363c
pradyumnasagar <pradyumna.j.ram@gmail.com>
2016-05-09 23:00:36 +0530

    welcome script changed

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
+cat welcome
+read answer
+if echo "$answer" | grep -iq "^y" ;then
 sh 0.autonames.sh
-
+else
+cat no.txt
+fi
 echo -n "We have some tools by default but please make sure you have all the other necessary tools(y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
