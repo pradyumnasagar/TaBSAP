f4ac854d827e93fdfb334bcc5aaa3dc5f59006a7
pradyumnasagar <pradyumna.j.ram@gmail.com>
2016-07-07 13:11:22 +0530

    pdf single page

--- heatmapcat.R
+++ heatmapcat.R
@@ -6,7 +6,7 @@ annotation_row = data.frame(
                     SampleType = factor(rep(c(grpname), c(grpcat)))
                 )
 rownames(annotation_row) = paste("sample", 1:totll, sep = "")
-pdf("mappp.pdf")
+pdf("mappp.pdf", onefile=FALSE)
 
 pheatmap(t2, cluster_rows=TRUE, cluster_cols=FALSE, annotation_row = annotation_row, fontsize=8)
 dev.off()
