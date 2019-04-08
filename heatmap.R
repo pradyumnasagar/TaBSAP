410d79cfe07c7df6666cb4845bd3c4121b32b771
Pradyumna <pradyumna.j@learner.manipal.edu>
2019-04-08 16:05:56 +0530

    making clean

--- heatmap.R
+++
@@ -1,8 +0,0 @@
-library(pheatmap)
-t1 <- read.table("mappp.tsv", header = TRUE, sep = "\t")
-t2 <- t1[,-1]
-row.names(t2) <- t1[,1]
-pdf("mappp.pdf", onefile=FALSE)
-pheatmap(t2, cluster_rows=FALSE, cluster_cols=FALSE, fontsize=8)
-dev.off()
-q()
